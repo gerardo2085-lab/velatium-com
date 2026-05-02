acc5a3f0d52fd4827e215b5f70f2b26477464bd5
gerardo2085-lab <gerardo2085@icloud.com>
2026-05-02 02:41:34 +0000

    Add publicDir configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://thevelatium.com',
   output: 'static',
+  publicDir: './public',
   build: {
     assets: 'assets'
   }
